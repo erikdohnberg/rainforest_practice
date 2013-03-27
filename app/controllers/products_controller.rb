36edc81f14087872f6ce67b2e0efd0cdef09e82a
Erik Dohnberg <erikdohnberg@gmail.com>
2013-03-27 09:17:07 -0400

    Removed unused view files. Wrote base Products Controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,22 +1,41 @@
 class ProductsController < ApplicationController
+  # GET product
   def index
+    @products = Product.all
   end
 
+  # GET product/1
   def show
+    @product = Product.find(params[:id])
   end
 
-  def edit
+  # GET product/new
+  def new
+    @product = Product.new
   end
 
+  # POST product
   def create
+    @product = Product.new(params[:product])
+    if @product.save
+      # redirect (move the browser) to /product/#{@product.id}
+      redirect_to @product, notice: 'Product posted successfully!'
+    else
+      render action: :new
   end
 
+  # GET product/1/edit
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  # GET product/1
   def update
+    @product = Product.find(params[:id])
   end
 
   def destroy
   end
 
-  def new
-  end
+
 end
