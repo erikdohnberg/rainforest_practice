bc83a59eadcfcb4bfdca2975fade3a32a365324f
Erik Dohnberg <erikdohnberg@gmail.com>
2013-03-27 14:05:18 -0400

    Added validations to product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :name, :price_in_cents
+
+  validates :name, :description, :price_in_cents, :presence => true
+  validates :price_in_cents, :numericality => {:greater_than_or_equal_to => 0}
+
 end
