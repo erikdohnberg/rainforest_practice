7ce8a34f3e72fbdd835b416ebc824fb636b1eace
Erik Dohnberg <erikdohnberg@gmail.com>
2013-03-27 16:35:15 -0400

    Wrote sessions controller, added current_user instance method in application controller, and created back_to_products partial

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,23 @@
 class SessionsController < ApplicationController
+  # GET us the login form
   def new
   end
 
-  def create
-  end
+  # POST a new session
+	def create
+		user = User.find_by_email(params[:email])
+		if user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect_to products_path, :notice => "Logged In"
+		else
+			flash.now.alert = "Invalid email or password"
+			render "new"
+		end
+	end
 
-  def destroy
-  end
+  # DELETE to end session
+	def destroy
+		session[:user_id] = nil
+		redirect_to root_url, :notice => "Logged Out"
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rainforest::Application.routes.draw do
   
-  get "sessions/new"
-
-  get "sessions/create"
-
-  get "sessions/destroy"
-
   root :to => "products#index"
 
   resources :products
   resources :users, :only => [:new, :create]
+  resources :sessions, :only => [:new, :create, :destroy]
 
 
   # The priority is based upon order of creation:
